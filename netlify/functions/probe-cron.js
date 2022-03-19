8e99f4ccf2e39e3783a6cc5998203f3105476a49
Ben Nadel <ben@bennadel.com>
2022-03-19 05:38:06 -0400

    Adding api calls and console logging. Still seeing how this all works.

--- netlify/functions/probe-cron.js
+++ netlify/functions/probe-cron.js
@@ -1,12 +1,37 @@
 
 // Import node modules.
+var axios = require( "axios" );
 
 // ----------------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------- //
 
 exports.handler = async function( event, context ) {
 
-	console.log( "Probe-cron called." );
+	try {
+
+		var apiResponse = await axios({
+			method: "get",
+			url: "https://www.bennadel.com/index.cfm?event=probe.healthcheck",
+			timeout: 3000
+		});
+
+		console.log( "Healthcheck passed." );
+
+	} catch ( error ) {
+
+		if ( error.response ) {
+
+			console.log( `Healthcheck returned with non-200 status code [${ error.response.status }].` );
+			console.log( "TODO: Call postmark API." );
+
+		} else {
+
+			console.log( "Healthcheck could not make outbound request." );
+			console.log( error.message );
+
+		}
+
+	}
 
 	return({
 		statusCode: 200,
